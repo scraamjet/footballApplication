951f7c400b617d25c38675393fc1eaa3564e4c8a
Rocketman887 <shuba.2001@inbox.ru>
2021-03-27 15:16:05 +0300

    API added

--- app/src/main/java/com/example/footballapplication/MainActivity.kt
+++ app/src/main/java/com/example/footballapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.footballapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    private fun swapFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fl_teams_list,fragment).
+        addToBackStack("app_stack").commit()
     }
 }
